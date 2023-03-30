f1c249421a55bc3b96b50b1cbf77429d73c39c16
Cristian Chiru <cristi.chiru@gmail.com>
2023-03-30 16:09:33 +0300

    Remove /auth path segments; bump gocd plugin to 20.3.0

--- docs/INSTALL.md
+++ docs/INSTALL.md
@@ -2,28 +2,29 @@
 
 ## Requirements
 
-* GoCD server version v17.5.0 or above
+* GoCD server version v20.3.0 or above
 * Keycloak [API documentation](https://www.keycloak.org/docs-api/11.0/rest-api/index.html)
 
 ## Installation
 
-Copy the file `build/libs/keycloak-oauth-authorization-plugin-VERSION.jar` to the GoCD server under `${GO_SERVER_DIR}/plugins/external` 
-and restart the server. The `GO_SERVER_DIR` is usually `/var/lib/go-server` on Linux and `C:\Program Files\Go Server` 
+Copy the file `build/libs/keycloak-oauth-authorization-plugin-VERSION.jar` to the GoCD server under `${GO_SERVER_DIR}/plugins/external`
+and restart the server. The `GO_SERVER_DIR` is usually `/var/lib/go-server` on Linux and `C:\Program Files\Go Server`
 on Windows.
 
 ## Configuration
+
 Provide details of the Keycloak server to connect to via an [Authorization Configuration](AUTHORIZATION_CONFIGURATION.md).
 
-###  Configure Keycloak Client
+### Configure Keycloak Client
 
 1. Sign in Keycloak Console
 2. Select the realm that you want to configure. Ex. **Master**
-3. Click in **Clients** menu 
+3. Click in **Clients** menu
 4. Click **Add** button
 5. On the form insert the client name
 6. On the next page, set this configs:
     1. In **Access Type** select **Confidential**
-    2. In **Valid Redirect URIs** insert the URL of GoCD, ex.: **http://localhost:8153**
+    2. In **Valid Redirect URIs** insert the URL of GoCD, ex.: **<http://localhost:8153>**
     3. In **Credentials** tab copy value of **Secret**
 
 ### Create Group Configuration
@@ -37,5 +38,5 @@ Provide details of the Keycloak server to connect to via an [Authorization Confi
     4. Select the **user** that you want to configure this role
     5. Select **Groups** tab and select the group in **Available Groups**
 
-> Obs.: By default Keycloak do not provide group definition on user session, to get this, edit 
+> Obs.: By default Keycloak do not provide group definition on user session, to get this, edit
 >**profile** scope and add groups in **Mappers** tab, thie scope needs to be added as builtin.
--- src/test/java/cd/go/authorization/keycloak/KeycloakApiClientTest.java
+++ src/test/java/cd/go/authorization/keycloak/KeycloakApiClientTest.java
@@ -73,7 +73,7 @@ public class KeycloakApiClientTest {
     public void shouldReturnAuthorizationServerUrl() throws Exception {
         final String authorizationServerUrl = KeycloakApiClient.authorizationServerUrl("call-back-url");
 
-        assertThat(authorizationServerUrl, startsWith("https://example.com/auth/realms/master/protocol/openid-connect/auth?client_id=client-id&redirect_uri=call-back-url&response_type=code&scope=openid%20profile%20email%20roles&state="));
+        assertThat(authorizationServerUrl, startsWith("https://example.com/realms/master/protocol/openid-connect/auth?client_id=client-id&redirect_uri=call-back-url&response_type=code&scope=openid%20profile%20email%20roles&state="));
     }
 
     @Test
@@ -89,7 +89,7 @@ public class KeycloakApiClientTest {
         assertThat(tokenInfo.accessToken(), is("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiYWRtaW4iOnRydWV9"));
 
         RecordedRequest request = server.takeRequest();
-        assertEquals("POST /auth/realms/master/protocol/openid-connect/token HTTP/1.1", request.getRequestLine());
+        assertEquals("POST /realms/master/protocol/openid-connect/token HTTP/1.1", request.getRequestLine());
         assertEquals("application/x-www-form-urlencoded", request.getHeader("Content-Type"));
         assertEquals("client_id=client-id&client_secret=client-secret&code=some-code&grant_type=authorization_code&redirect_uri=callback-url", request.getBody().readUtf8());
     }
@@ -108,7 +108,7 @@ public class KeycloakApiClientTest {
         assertThat(KeycloakUser.getEmail(), is("foo@bar.com"));
 
         RecordedRequest request = server.takeRequest();
-        assertEquals("GET /auth/realms/master/protocol/openid-connect/userinfo HTTP/1.1", request.getRequestLine());
+        assertEquals("GET /realms/master/protocol/openid-connect/userinfo HTTP/1.1", request.getRequestLine());
         assertEquals("Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiYWRtaW4iOnRydWV9", request.getHeader("Authorization"));
     }
 
@@ -121,7 +121,7 @@ public class KeycloakApiClientTest {
         when(KeycloakConfiguration.keycloakEndpoint()).thenReturn(server.url("/").toString());
 
         thrown.expect(RuntimeException.class);
-        thrown.expectMessage("Api call to `/auth/realms/master/protocol/openid-connect/userinfo` failed with error: `Unauthorized`");
+        thrown.expectMessage("Api call to `/realms/master/protocol/openid-connect/userinfo` failed with error: `Unauthorized`");
 
         KeycloakApiClient.userProfile(tokenInfo);
     }
