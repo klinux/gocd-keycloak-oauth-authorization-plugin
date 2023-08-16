81ff73d611fe7446924145a1624d5ee0d40f63f0
Kleber Rocha <klinux@gmail.com>
2023-08-16 02:51:43 -0300

    Fix: some tests
    Signed-off-by: Kleber Rocha <klinux@gmail.com>

--- src/test/java/cd/go/authorization/keycloak/executors/GetPluginIconRequestExecutorTest.java
+++ src/test/java/cd/go/authorization/keycloak/executors/GetPluginIconRequestExecutorTest.java
@@ -25,7 +25,7 @@ import java.util.HashMap;
 
 import static java.util.Base64.getDecoder;
 import static org.hamcrest.CoreMatchers.is;
-import static org.junit.Assert.assertThat;
+import static org.hamcrest.MatcherAssert.assertThat;
 
 public class GetPluginIconRequestExecutorTest {
 
